392f0a079f1a7b701ec4be578fe1e371e0efc860
Zet <zet.pick@gmail.com>
2025-11-26 03:41:41 +0500

    feat: fix migrations

--- saleor/story/migrations/0001_initial.py
+++ saleor/story/migrations/0001_initial.py
@@ -8,9 +8,7 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-        ('core', '0001_initial'),
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
