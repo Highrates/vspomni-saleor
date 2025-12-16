6d51474bcf5a4390e97979335103c66617156378
Zet Web <zet.pick@gmail.com>
2025-12-16 23:55:14 +0500

    fix: migrations

--- saleor/account/migrations/0097_add_phone_to_emailverificationcode.py
+++ saleor/account/migrations/0097_add_phone_to_emailverificationcode.py
@@ -10,10 +10,24 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
+        migrations.CreateModel(
+            name='EmailVerificationCode',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('email', models.EmailField(db_index=True, max_length=254)),
+                ('code', models.CharField(db_index=True, max_length=6)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('is_used', models.BooleanField(default=False)),
+                ('phone', models.CharField(blank=True, default='', max_length=20)),
+            ],
+            options={
+                'verbose_name': 'Email verification code',
+                'verbose_name_plural': 'Email verification codes',
+            },
+        ),
+        migrations.AddIndex(
             model_name='emailverificationcode',
-            name='phone',
-            field=models.CharField(blank=True, default='', max_length=20),
+            index=models.Index(fields=['email', 'created_at'], name='account_ema_email_cr_idx'),
         ),
     ]
 
